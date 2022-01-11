e2668e68bf0013d5c83fddae085b3035b9626645
Kartik3012 <83570622+Kartik3012@users.noreply.github.com>
2022-01-11 15:45:25 +0530

    Add files via upload

---
+++ String_Reverse.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+#include<string>
+
+    //1110
+    //  01
+string reverse_string(string s)
+{
+    int n=s.length();
+    for(int i=0,j=n-1;i<n/2,j>=n/2;i++,j--)
+    {
+        swap(s[i],s[j]);
+    }
+    return s;
+}   
+
+int main()
+{
+    cout<<"ENTER A STRING TO REVERSE:"<<endl;
+    string str;
+    cin>>str;
+
+    cout<<"ORIGINAL STRING: "<<str<<endl;
+    str=reverse_string(str);
+    cout<<"REVERSED STRING: "<<str<<endl;
+    return 0;
+}
